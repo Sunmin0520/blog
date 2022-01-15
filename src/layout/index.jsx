07c2765f360220c01d838e4d6c7affe140725469
Sunmin0520 <sunmin.dev@gmail.com>
2022-01-15 13:48:20 +0900

    update: width 넓게 수정

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -17,7 +17,7 @@ export const Layout = ({ location, title, children }) => {
         style={{
           marginLeft: `auto`,
           marginRight: `auto`,
-          maxWidth: rhythm(37),
+          maxWidth: rhythm(45),
           padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
         }}
       >

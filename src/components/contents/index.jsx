5dfff7f7e5a5decdce817f7c393dbd91f2c26bda
Sunmin0520 <sunmin.dev@gmail.com>
2023-02-16 22:00:56 +0900

    fix: 랜덤 카테고리의 글 가져오는 것을 반복문으로 바꾸어 코드 정리

--- src/components/contents/index.jsx
+++ src/components/contents/index.jsx
@@ -6,80 +6,41 @@ import { CATEGORY_TYPE } from '../../constants'
 
 export const Contents = ({ posts, countOfInitialPost, count, category }) => {
   const refinedPosts = useMemo(() => {
+    let allCategoryPosts = []
+    let temp = []
     let filteredPosts = posts.filter(
       ({ node }) =>
         category === CATEGORY_TYPE.ALL || node.frontmatter.category === category
     )
 
     if (category === CATEGORY_TYPE.RANDOM) {
-      const nodejsPosts = posts.filter(
-        ({ node }) => node.frontmatter.category === 'nodejs'
+      const categories = [
+        'nodejs',
+        'retrospective',
+        'javascript',
+        'web',
+        'git',
+        'cs',
+        'docker',
+        'books',
+        'security',
+        'etc.',
+        'database',
+        'oop',
+        'reference',
+      ]
+
+      const randomlySortedCategories = categories.sort(
+        () => Math.random() - 0.5
       )
 
-      const retrospectivePosts = posts.filter(
-        ({ node }) => node.frontmatter.category === 'retrospective'
-      )
-
-      const javascriptPosts = posts.filter(
-        ({ node }) => node.frontmatter.category === 'javascript'
-      )
-
-      const webPosts = posts.filter(
-        ({ node }) => node.frontmatter.category === 'web'
-      )
-
-      const gitPosts = posts.filter(
-        ({ node }) => node.frontmatter.category === 'git'
-      )
-
-      const csPosts = posts.filter(
-        ({ node }) => node.frontmatter.category === 'cs'
-      )
-
-      const dockerPosts = posts.filter(
-        ({ node }) => node.frontmatter.category === 'docker'
-      )
-
-      const booksPosts = posts.filter(
-        ({ node }) => node.frontmatter.category === 'books'
-      )
-
-      const securityPosts = posts.filter(
-        ({ node }) => node.frontmatter.category === 'security'
-      )
-
-      const etcPosts = posts.filter(
-        ({ node }) => node.frontmatter.category === 'etc.'
-      )
-
-      const databasePosts = posts.filter(
-        ({ node }) => node.frontmatter.category === 'database'
-      )
-
-      const oopPosts = posts.filter(
-        ({ node }) => node.frontmatter.category === 'oop'
-      )
-
-      const referencePosts = posts.filter(
-        ({ node }) => node.frontmatter.category === 'reference'
-      )
-
-      const allCategoryPosts = nodejsPosts
-        .concat(
-          retrospectivePosts,
-          javascriptPosts,
-          webPosts,
-          gitPosts,
-          csPosts,
-          dockerPosts,
-          booksPosts,
-          securityPosts,
-          etcPosts,
-          databasePosts,
-          oopPosts,
-          referencePosts
+      randomlySortedCategories.forEach(category => {
+        temp = posts.filter(
+          ({ node }) => node.frontmatter.category === category
         )
-        .flat()
+        posts.concat(temp)
+      })
+      allCategoryPosts = posts.flat()
 
       let randomPosts = []
       while (randomPosts.length < countOfInitialPost) {
